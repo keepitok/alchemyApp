0684fc30d6eef111875f1f6dda7e5c10c426cc8c
Kuvakin Sergey <kuvakin@gmail.com>
2014-11-27 17:04:51 +0300

    refs #6524

--- backend/index.js
+++ backend/index.js
@@ -34,84 +34,99 @@ var getAlchemyApp = function (obj) {
     };
 
 var setData = function (req, res) {
-    var currentInterests;
-    request.get(getSettingsApp({
-        groupId: vars.groupId,
-        bucketName: vars.bucket,
-        apps: vars.appName,
-        appKey: vars.appKey
-    }), {
-        auth: vars.auth
-    }, function (error, response) {
-        var apiKey = JSON.parse(response.body).custom.apiKey;
-        var profile = req.body.profile;
-
-        // parsing the profile to get URL and Profile ID
-        var session = profile.sessions[0],
-            url = session.events[0].data['page-url'],
-            profileId = profile.id;
-        console.log('URL visited: ' + url);
-        console.log('Profile ID: ' + profileId);
-
-        // making the entity extraction call
-        request.get(getAlchemyApp({
-            apikey: apiKey,
-            url: url
-        }), function (error, response) {
-            response = JSON.parse(response.body);
-            var interests = [];
-            for (var i = 0; i < response.entities.length; i++) {
-                var entitie = response.entities[i];
-                if (entitie.type === 'FieldTerminology') {
-                    interests.push(entitie.text);
-                }
-                if (interests.length >= 3) {
-                    break;
-                }
+    try {
+        var currentInterests;
+        request.get(getSettingsApp({
+            groupId: vars.groupId,
+            bucketName: vars.bucket,
+            apps: vars.appName,
+            appKey: vars.appKey
+        }), {
+            auth: vars.auth
+        }, function (error, response) {
+            if (error && !response.body) {
+                throw error || new Error('Empty response');
             }
-            console.log('interests: ' + interests);
-            // getting the profile to check current interests
-            request.get(getProfilesApp({
-                groupId: /*vars.groupId*/ 222,
-                bucketName: /*vars.bucket*/ 'first-bucket',
-                profileId: /*profileId*/ 'xjf8k76t1d7n807lhp8yjwqjbmw0j9sq',
-                appKey: vars.appKey
+            var apiKey = JSON.parse(response.body).custom.apiKey;
+            var profile = req.body.profile;
+
+            // parsing the profile to get URL and Profile ID
+            var session = profile.sessions[0],
+                url = session.events[0].data['page-url'],
+                profileId = profile.id;
+            // console.log('URL visited: ' + url);
+            // console.log('Profile ID: ' + profileId);
+
+            // making the entity extraction call
+            request.get(getAlchemyApp({
+                apikey: apiKey,
+                url: url
             }), function (error, response) {
-                var receivedProfile = JSON.parse(response.body).profile;
-                console.log('Received profile object: ' + receivedProfile);
-                if (receivedProfile.attributes && receivedProfile.attributes[0] && receivedProfile.attributes[0].data.interests) {
-                    currentInterests = receivedProfile.attributes[0].data.interests;
-                } else {
-                    currentInterests = [];
+                if (error && !response.body) {
+                    throw error || new Error('Empty response');
                 }
-                console.log('current interests: ' + currentInterests);
-                interests = _.uniq(currentInterests.concat(interests));
-                console.log('merged interests: ' + interests);
-                request.post({
-                    url: getProfilesApp({
-                        groupId: /*vars.groupId*/ 222,
-                        bucketName: /*vars.bucket*/ 'first-bucket',
-                        profileId: /*profileId*/ 'xjf8k76t1d7n807lhp8yjwqjbmw0j9sq',
-                        appKey: vars.appKey
-                    }),
-                    body: {
-                        id: profileId,
-                        attributes: [{
-                            collectApp: session.collectApp,
-                            section: session.section,
-                            data: {
-                                interests: interests
-                            }
-                        }]
-                    },
-                    json: true
-                }, function (error, response) {
-                    console.log(response.body);
-                    res.send(null);
+                response = JSON.parse(response.body);
+                var interests = [];
+                for (var i = 0; i < response.entities.length; i++) {
+                    var entitie = response.entities[i];
+                    if (entitie.type === 'FieldTerminology') {
+                        interests.push(entitie.text);
+                    }
+                    if (interests.length >= 3) {
+                        break;
+                    }
+                }
+                // console.log('interests: ' + interests);
+                // getting the profile to check current interests
+                request.get(getProfilesApp({
+                    groupId: /*vars.groupId*/ 222,
+                    bucketName: /*vars.bucket*/ 'first-bucket',
+                    profileId: /*profileId*/ 'xjf8k76t1d7n807lhp8yjwqjbmw0j9sq',
+                    appKey: vars.appKey
+                }), function (error, response) {
+                    if (error && !response.body) {
+                        throw error || new Error('Empty response');
+                    }
+                    var receivedProfile = JSON.parse(response.body).profile;
+                    if (receivedProfile.attributes && receivedProfile.attributes[0] && receivedProfile.attributes[0].data.interests) {
+                        currentInterests = receivedProfile.attributes[0].data.interests;
+                    } else {
+                        currentInterests = [];
+                    }
+                    // console.log('current interests: ' + currentInterests);
+                    interests = _.uniq(currentInterests.concat(interests));
+                    // console.log('merged interests: ' + interests);
+                    request.post({
+                        url: getProfilesApp({
+                            groupId: /*vars.groupId*/ 222,
+                            bucketName: /*vars.bucket*/ 'first-bucket',
+                            profileId: /*profileId*/ 'xjf8k76t1d7n807lhp8yjwqjbmw0j9sq',
+                            appKey: vars.appKey
+                        }),
+                        body: {
+                            id: profileId,
+                            attributes: [{
+                                collectApp: session.collectApp,
+                                section: session.section,
+                                data: {
+                                    interests: interests
+                                }
+                            }]
+                        },
+                        json: true
+                    }, function (error, response) {
+                        if (error && !response.body) {
+                            throw error || new Error('Empty response');
+                        }
+                        // console.log(response.body);
+                        res.send(null);
+                    });
                 });
             });
         });
-    });
+    } catch (e) {
+        res.send(e.message);
+    }
 };
 
 app.post('/', setData);
