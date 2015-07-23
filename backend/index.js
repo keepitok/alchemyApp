640b0df0fde0833379663b49995ccd3ec73a36de
ernil <morion.ernil@gmail.com>
2015-07-23 10:45:16 +0300

    refs 8368 | main functionality

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
  * If your app's frontend part is going to communicate directly with backend, you need to allow this
  * https://en.wikipedia.org/wiki/Cross-origin_resource_sharing
  */
+
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     next();
@@ -34,28 +35,62 @@ var vars = {
 var innoHelper = new inno.InnoHelper(vars);
 
 // POST request to "/" is always expected to recieve stream with events
-app.post('/', function (req, res) {
+app.post('/', function (req) {
 
     var profile = innoHelper.getProfileFromRequest(req.body);
     var session = profile.getLastSession();
     var events  = session.getEvents();
     var event   = events[0];
-    var url     = event.getDataValue('url');
+    var url     = event.getDataValue('page-url');
 
     innoHelper.getAppSettings(function (err, settings) {
-        
-        var alchemyUrl = 'http://access.alchemyapi.com/calls/url/URLGetRankedNamedEntities?apikey=' + settings.api_key + '&url=' + url + '&outputMode=json';
+
+        var alchemyUrl = 'http://access.alchemyapi.com/calls/url/URLGetRankedNamedEntities?' +
+            'apikey=' + settings.api_key +
+            '&url=' + url +
+            '&outputMode=json';
+
         request.get(alchemyUrl, function (err, res) {
-            
+
             var result = JSON.parse(res.body);
-            console.log(result);
-            
+            var section = session.getSection();
+            var interests = getInterests(result.entities, settings);
+
+            innoHelper.loadProfile(profile.getId(), function (err, fullProfile) {
+
+                interests.forEach(function (item) {
+                    if (item.relevance >= settings.minRelevance) {
+
+                        var attribute = fullProfile.getAttribute(item.text, innoHelper.getCollectApp(), section);
+                        var count = parseInt(item.count, 10);
+
+                        if (!attribute) {
+                            attribute = new inno.Profile.Attribute({
+                                name: item.text,
+                                value: count,
+                                section: section
+                            });
+                            fullProfile.setAttribute(attribute);
+                        } else {
+                            var current = attribute.getValue();
+                            attribute.setValue((current + count) / 2);
+                        }
+                    }
+                });
+
+                innoHelper.saveProfile(fullProfile, function (err, result) {
+                    console.log(err, result);
+                });
+            });
         });
     });
-
-    return res.json(url);
 });
 
+var getInterests = function (entities, settings) {
+    return entities.filter(function (item) {
+        return (settings.entityType.indexOf(item.type) > -1) && (parseFloat(item.relevance) >= settings.minRelevance);
+    }).splice(0, settings.amount);
+};
 
 // Starting server
 var server = app.listen(port, function () {
